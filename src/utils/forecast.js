5db0d230b6ab6b5dee135f1796d77de0054ebbd1
Burhanuddin Savliwala <burhanuddinsavli@gmail.com>
2021-08-30 18:24:50 +0530

    added humidity

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,15 +1,15 @@
 const request = require("request");
 
-const forecast = ({ latitude , longitude} = {}, callback) => {
+const forecast = ({ latitude, longitude } = {}, callback) => {
   const url = `http://api.weatherstack.com/current?access_key=cbb7e843738a7ee4669ee7e2471c77c8&query=${latitude},${longitude}&units=f`;
-  request({url, json: true }, (error, {body:response}) => {
+  request({ url, json: true }, (error, { body: response }) => {
     if (error) {
       callback("Unable to connect to weather api", undefined);
     } else if (response.error) {
       callback("Unable to find location", undefined);
     } else {
       const currdata = response.current;
-      const msg = `${currdata.weather_descriptions[0]} .It is currently ${currdata.temperature} degrees farhenheit but it feels like it is ${currdata.feelslike}`;
+      const msg = `${currdata.weather_descriptions[0]} .It is currently ${currdata.temperature} degrees farhenheit but it feels like it is ${currdata.feelslike}. Also the humidity is ${currdata.humidity}`;
       callback(undefined, msg);
     }
   });
